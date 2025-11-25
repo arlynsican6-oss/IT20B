3bb2570d3b89152449abcb3c30eef971889af0b4
arlynsican6-oss <arlynsican6@gmail.com>
2025-11-25 10:52:26 +0800

    Added update method for DSA/array.js

--- DSA/array.js
+++ DSA/array.js
@@ -56,13 +56,28 @@ class Array{
         this.length--;
 
     }
+
+    update(index,element){
+        //Correcting Statement || Must be false
+        if(index <0 || index >= this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //After Condition
+        this.data[index] = element;
+    }
 }
 
+
 //
-arr1 = new Array([4,2,3]);
 
 arr1.traverse();
 arr1.insert(0,9);
 arr1.traverse();
+
 arr1.delete(1);
+arr1.traverse();
+
+arr1.update(2,5);
 arr1.traverse();
